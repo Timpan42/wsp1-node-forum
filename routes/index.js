be79ea688ef2dcca3eb545ec25d42f19703be0eb
Timpan42 <tim.fagerdal@elev.ga.ntig.se>
2023-01-24 14:43:52 +0100

    slutet av del 8. funkar inte att skicka data :(

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,42 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', async function (req, res) {
-    res.render('index.njk')
+    res.render('index.njk',{
+        title:'Home'
+    })
 });
 
 router.post('/new', async function (req, res, next) {
     const { author, title, content } = req.body;
-    const [rows] = await promisePool.query("INSERT INTO tf03forum (authorId, title, content) VALUES (?, ?, ?)", [author, title, content]);
-    res.redirect('/');
+
+    // Skapa en ny författare om den inte finns men du behöver kontrollera om användare finns!
+    let user = await promisePool.query('SELECT * FROM tf03users WHERE name = ?', [author]);
+    if (!user) {
+        user = await promisePool.query('INSERT INTO tf03users (name) VALUES (?)', [author]);
+    }
+
+    // user.insertId bör innehålla det nya ID:t för författaren
+
+    const userId = user.insertId || user[0].id;
+
+    // kör frågan för att skapa ett nytt inlägg
+    const [rows] = await promisePool.query('INSERT INTO tf03forum (author, title, content) VALUES (?, ?, ?)', [userId, title, content]);
+    res.redirect('/'); // den här raden kan vara bra att kommentera ut för felsökning, du kan då använda tex. res.json({rows}) för att se vad som skickas tillbaka från databasen
 });
 
+
+// router.post('/new', async function (req, res, next) {
+//     const { author, title, content } = req.body;
+//     const [rows] = await promisePool.query("INSERT INTO tf03forum (authorId, title, content) VALUES (?, ?, ?)", [author, title, content]);
+//     res.redirect('/');
+
+// });
+
 router.get('/new', async function (req, res, next) {
+    const [users] = await promisePool.query('SELECT * FROM tf03users');
     res.render('new.njk', {
         title: 'Nytt inlägg',
+        users,
     });
 });
 
