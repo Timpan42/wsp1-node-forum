beaa199310365cdb4916074b4d6b499661ca8a9a
Timpan42 <tim.fagerdal@elev.ga.ntig.se>
2023-01-27 09:25:30 +0100

    del 8 funkar nu med post i home

--- routes/index.js
+++ routes/index.js
@@ -1,46 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async function (req, res) {
-    res.render('index.njk',{
-        title:'Home'
-    })
-});
-
-router.post('/new', async function (req, res, next) {
-    const { author, title, content } = req.body;
-
-    // Skapa en ny författare om den inte finns men du behöver kontrollera om användare finns!
-    let user = await promisePool.query('SELECT * FROM tf03users WHERE name = ?', [author]);
-    if (!user) {
-        user = await promisePool.query('INSERT INTO tf03users (name) VALUES (?)', [author]);
-    }
-
-    // user.insertId bör innehålla det nya ID:t för författaren
-
-    const userId = user.insertId || user[0].id;
-
-    // kör frågan för att skapa ett nytt inlägg
-    const [rows] = await promisePool.query('INSERT INTO tf03forum (author, title, content) VALUES (?, ?, ?)', [userId, title, content]);
-    res.redirect('/'); // den här raden kan vara bra att kommentera ut för felsökning, du kan då använda tex. res.json({rows}) för att se vad som skickas tillbaka från databasen
-});
-
-
-// router.post('/new', async function (req, res, next) {
-//     const { author, title, content } = req.body;
-//     const [rows] = await promisePool.query("INSERT INTO tf03forum (authorId, title, content) VALUES (?, ?, ?)", [author, title, content]);
-//     res.redirect('/');
-
-// });
-
-router.get('/new', async function (req, res, next) {
-    const [users] = await promisePool.query('SELECT * FROM tf03users');
-    res.render('new.njk', {
-        title: 'Nytt inlägg',
-        users,
-    });
-});
-
 const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
@@ -50,14 +10,36 @@ const pool = mysql.createPool({
 });
 const promisePool = pool.promise();
 
+module.exports = router;
+
 router.get('/', async function (req, res, next) {
     const [rows] = await promisePool.query("SELECT * FROM tf03forum");
-    res.json({ rows });
-
-    res.render('/index.njk', {
+    res.render('index.njk', {
         rows: rows,
         title: 'Forum',
     });
 });
 
-module.exports = router;
+router.post('/new', async function (req, res, next) {
+    const { author, title, content } = req.body;
+
+    let user = await promisePool.query('SELECT * FROM tf03users WHERE name = ?', [author]);
+    if (!user) {
+        user = await promisePool.query('INSERT INTO tf03users (name) VALUES (?)', [author]);
+    }
+
+    console.log(user)
+
+    const userId = user.insertId || user[0][0].id;
+
+    const [rows] = await promisePool.query('INSERT INTO tf03forum (authorId, title, content) VALUES (?, ?, ?)', [userId, title, content]);
+    res.redirect('/');
+});
+
+router.get('/new', async function (req, res, next) {
+    const [users] = await promisePool.query('SELECT * FROM tf03users');
+    res.render('new.njk', {
+        title: 'Nytt inlägg',
+        users,
+    });
+});
